62b640eb81ef8d15f43d1cb68fb6a2973d4f3759
Tu <huynhtu.1024@gmail.com>
2021-03-15 20:09:20 +0700

    Use beforeMethod and afterMethod

--- src/test/java/SignInTest.java
+++ src/test/java/SignInTest.java
@@ -1,47 +1,80 @@
-import org.junit.Assert;
-import org.junit.Test;
+
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class SignInTest {
-    private final String driver = "webdriver.chrome.driver";
-    private final String linkDriver = "C:\\chromedriver.exe";
+
+    Logger logger = Logger.getLogger(SignInTest.class);
     private final String baseURL = "https://github.com/";
+
+    public WebDriver webDriver;
+
+    public Properties getPropertyValue() {
+        Properties prop = new Properties();
+        String propFileName = "config.properties";
+
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+        if (inputStream != null) {
+            try {
+                prop.load(inputStream);
+                return prop;
+            } catch (IOException e) {
+                logger.error("Throw IOException");
+            }
+        }
+        return null;
+    }
+
+    @BeforeMethod
+    public void getDriver() {
+        System.setProperty(getPropertyValue().getProperty("driver"), getPropertyValue().getProperty("linkDriver"));
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void closeDrive() {
+        webDriver.quit();
+    }
 
     @Test
     public void testGetHomePage() {
-        System.setProperty(driver, linkDriver);
-        ChromeDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-
-        driver.get(baseURL);
+        webDriver.get(baseURL);
         String expectedTitle = "GitHub: Where the world builds software · GitHub";
-        Assert.assertEquals(expectedTitle, driver.getTitle());
-        Assert.assertEquals(baseURL, driver.getCurrentUrl());
+        Assert.assertEquals(expectedTitle, webDriver.getTitle());
+        Assert.assertEquals(baseURL, webDriver.getCurrentUrl());
     }
 
     @Test
     public void testSignInPage() {
-        System.setProperty(driver, linkDriver);
-        ChromeDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        webDriver.get(baseURL);
+        webDriver.findElement(By.linkText("Sign in")).click();
 
-        driver.get(baseURL);
-        driver.findElement(By.linkText("Sign in")).click();
+        WebElement username = webDriver.findElement(By.xpath("//input[@name='login']"));
+        username.sendKeys(getPropertyValue().getProperty("username"));
 
-        WebElement username = driver.findElement(By.xpath("//input[@name='login']"));
-        username.sendKeys("PTHuynhTu");
+        WebElement password = webDriver.findElement(By.xpath("//input[@name='password']"));
+        password.sendKeys(getPropertyValue().getProperty("password"));
 
-        WebElement password = driver.findElement(By.xpath("//input[@name='password']"));
-        password.sendKeys("Tu@0906062717");
-
-        WebElement signInButton = driver.findElement(By.xpath("//input[@name='commit']"));
+        WebElement signInButton = webDriver.findElement(By.xpath("//input[@name='commit']"));
         signInButton.click();
 
         String expectedTitle = "GitHub";
-        Assert.assertEquals(expectedTitle, driver.getTitle());
-        Assert.assertEquals(baseURL, driver.getCurrentUrl());
+        Assert.assertEquals(expectedTitle, webDriver.getTitle());
+        Assert.assertEquals(baseURL, webDriver.getCurrentUrl());
+
     }
 
 }
